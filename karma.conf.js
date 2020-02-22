579376d1eaef3196942e4d84b7a6b3b2830dd9b2
mcanoa <mateocano31@gmail.com>
2020-02-22 15:13:06 -0500

    AddChromeHeadless

--- karma.conf.js
+++ karma.conf.js
@@ -1,8 +1,4 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
-
 const process = require('process');
-
 process.env.CHROME_BIN = require('puppeteer').executablePath();
 
 module.exports = function(config) {
@@ -30,8 +26,10 @@ module.exports = function(config) {
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: true,
-        browsers: ['ChromeHeadLess'],
+        browsers: ['ChromeHeadless'],
         singleRun: false,
-        restartOnFileChange: true
+        restartOnFileChange: true,
+        concurrency: Infinity
+
     });
 };
